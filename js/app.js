2e804925520538d7df4c53333d4bbba7e997b3b9
barachenadia <barache.nadia@yahoo.fr>
2015-02-15 16:20:04 +0100

    Form with Angular JS

--- js/app.js
+++ js/app.js
@@ -9,10 +9,33 @@
     app.controller('StoreController', function(){
         this.products = books ;
     });
+    app.controller('GalleryController', function(){
+      this.current = 0;
+      this.SetCurrent = function(newGallery) {
+        this.current = newGalery || 0;
+      }
+    });
 
+    app.controller('TabController', function(){
 
+  this.tab = 1;
 
+  this.setTab = function(selectTab) {
+    this.tab = selectTab;
+  };
 
+  this.isSet = function(selectedTab){
+    return this.tab === selectedTab;
+  };
+    });
+    app.controller('ReviewController', function(){
+      this.review = {};
+      this.addReview = function(product){
+        this.review.createdOn = Date.now();
+        product.reviews.push(this.review);
+        this.review = {};
+      }
+    });
 
 var books = [
 
@@ -20,13 +43,26 @@ var books = [
      name : ' Laissée pour morte' ,
      price : 6.95 ,
      description : ' Un témoignage poignant et courageux. ',
+
      canPurchase : true,
+     shine: 8,
      soldeOut : false,
      images: [
           "images/gem-02.gif",
           "images/gem-05.gif",
           "images/gem-09.gif"
-    ]
+        ],
+    reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org",
+        createdOn: 1397490980837
+      }]
  },
 
 {
@@ -34,12 +70,25 @@ var books = [
      price : 22.99 ,
      description : ' Cheryl Strayed nous entraîne grâce à ce récit humain et bouleversant sur les chemins. ',
      canPurchase : true,
+     shine: 9,
      soldeOut : false,
      images: [
       "images/gem-01.gif",
       "images/gem-03.gif",
       "images/gem-04.gif"
-    ]
+    ],
+
+    reviews: [{
+        stars: 3,
+        body: "I think this gem was just OK, could honestly use more shine, IMO.",
+        author: "JimmyDean@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 4,
+        body: "Any gem with 12 faces is for me!",
+        author: "gemsRock@example.org",
+        createdOn: 1397490980837
+      }]
  }
 
  ];
